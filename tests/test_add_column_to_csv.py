33a9208d74c69eba032cd2f0681afb4c4e62ee29
Rhianwen Davies <106972460+RhiDaviesFathom@users.noreply.github.com>
2022-08-25 12:07:20 +0100

    Add csv columns script (#1)

---
+++ tests/test_add_column_to_csv.py
@@ -0,0 +1,8 @@
+from unittest.mock import patch
+from useful_scripts import add_column_to_csv
+
+
+@patch("useful_scripts.add_column_to_csv.add_column_in_csv")
+def test_main(mock_add_column_to_csv):
+    add_column_to_csv.add_column_in_csv()
+    assert mock_add_column_to_csv.called
