b19afe2069314a645d6949bd49ab6db96d329c84
Rhianwen Davies <106972460+RhiDaviesFathom@users.noreply.github.com>
2022-09-02 15:56:41 +0100

    Adding new script that will create a parquet file.

--- README.md
+++ README.md
@@ -6,9 +6,20 @@ one and might come in hand again later.
 
 ## Commands
 ### Add columns to an existing csv
-Edit the source code before running this, so it is relevant for your use case.
+
+This script takes two command line arguments
+1) The name of the csv file you want to add the columns to.
+2) The name of the output csv file you want to create.
+
+
 ```shell
 python3 add_column_to_csv.py
+```
+
+### Create or edit a parquet file
+Edit the source code before running this, so it is relevant for your use case.
+```shell
+python3 editing_parquets.py
 ```
 
 ## Installation/ Getting Started
---
+++ useful_scripts/editing_parquets.py
@@ -0,0 +1,63 @@
+"""
+Gives the ability to edit and create parquet files
+"""
+import os
+import pathlib
+import shutil
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+
+def _get_absolute_path_from_relative_path(relative_path: str) -> str:
+    """
+    Given a relative path (relative_path) to X from a file (test_file) this
+    returns the absolute path to X
+    """
+    file_dir = pathlib.Path(__file__).parent.resolve()
+    return f"{file_dir}/{relative_path}"
+
+
+def _safe_mkdir(relative_path: str) -> None:
+    """
+    Makes directory if it doesn't exist. If it does, then deletes the directory and recreates it.
+    """
+    path = _get_absolute_path_from_relative_path(relative_path)
+    try:
+        os.stat(path)
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        pass
+    os.mkdir(path)
+
+
+def create_catchment_parquet():
+
+    df = pd.DataFrame(
+        {
+            "lat_id": [184065, 184065],
+            "lon_id": [-13481, -13480],
+            "peril_id": ["ORF", "ORF"],
+            "return_period": [5, 5],
+            "flood_depth_cm": [85, 57],
+        }
+    )
+
+    table = pa.Table.from_pandas(df)
+    dir_path = _get_absolute_path_from_relative_path("output")
+    pq.write_table(table, f"{dir_path}/catchment_290.parquet")
+
+
+def main():
+    """
+    Comment out the unnecessary bit so that when editing_parquet.py from the command line, the desired behaviour occurs.
+    """
+
+    # copy_parquet()
+    create_catchment_parquet()
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
